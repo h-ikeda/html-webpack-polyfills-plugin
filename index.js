fdeb31c66f91e1db5c34022e97a326a3ea044dc4
IKEDA Hiroki <ikeda.hiroki.123@gmail.com>
2017-08-28 02:14:28 +0000

    Support custom features.

--- index.js
+++ index.js
@@ -12,7 +12,32 @@ module.exports = class {
                 let url = "https://cdn.polyfill.io/v2/polyfill";
                 url += ("minify" in this.options && !this.options.minify) ? ".": ".min.";
                 url += "type" in this.options ? this.options.type: "js";
-                let query = "";
+                let features;
+                if ("features" in this.options) {
+                    if (Array.isArray(this.options.features)) {
+                        features = this.options.features.map(feature => {
+                            if (typeof feature === "object") {
+                                let flags;
+                                if ("flags" in feature) {
+                                    flags = Array.isArray(feature.flags) ? feature.flags.join("|"): feature.flags;
+                                }
+                                if (flags) {
+                                    return feature.feature + "|" + flags;
+                                }
+                                return feature.feature;
+                            }
+                            return feature;
+                        }).join();
+                    } else {
+                        features = this.options.features;
+                    }
+                }
+                if (features) {
+                    url += "?";
+                    if (features) {
+                        url += "features=" + features;
+                    }
+                }
                 htmlPluginData.assets.js.unshift(url);
                 callback(null, htmlPluginData);
             });
